fc83c000fdbed5757748fd8fbd9c94368bc5335d
Philip Youssef <philip.youssef@gmail.com>
2017-04-25 16:43:55 -0700

    more koans

--- src/test/scala/org/functionalkoans/forscala/AboutEmptyValues.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutEmptyValues.scala
@@ -60,7 +60,7 @@ class AboutEmptyValues extends KoanSuite {
   koan("Option.getOrElse can be used to provide a default in the case of None") {
     val optional: Option[String] = Some("Some Value")
     val optional2: Option[String] = None
-    assert(optional.getOrElse("No Value") === __, "Should return the value in the option")
-    assert(optional2.getOrElse("No Value") === __, "Should return the specified default value")
+    assert(optional.getOrElse("No Value") === "Some Value", "Should return the value in the option")
+    assert(optional2.getOrElse("No Value") === "No Value", "Should return the specified default value")
   }
 }
